965c13abb05ea5080a95b3d63485b2af21e6ffaf
chrispaynes <cdpaynes@gmail.com>
2016-09-24 14:58:24 -0500

    Adds indexing and a header

--- cowboy_blog/lib/cowboy_blog/handler.ex
+++ cowboy_blog/lib/cowboy_blog/handler.ex
@@ -20,7 +20,30 @@ defmodule CowboyBlog.Handler do
     {:ok, req, state}
   end
 
-  # get_file/3 returns a list a the contents in the "priv/contents/".
+  # write_index/2 defines the function's default values.
+  def write_index(input_list, list \\ "")
+
+  # write_index/2 uses recursion to create an HTML list of links.
+  def write_index [head|tail], list do
+    ref = Path.rootname(head) |> String.capitalize
+    links = "<a href='/#{ref}'>#{ref}</a><br>"
+    write_index tail, list <> links
+  end
+
+  # write_index/2 returns a string of HTML links
+  def write_index [], list do
+    list
+  end
+    
+
+  def body_header do
+    file_list = File.ls! "priv/contents/"
+    index = write_index(file_list)
+    IO.inspect index
+    EEx.eval_file "priv/themes/header.html.eex", [title: "Cowboy Blog", index: index]
+  end
+
+  # get_file/3 returns a list of the contents in the "priv/contents/".
   # folder when an undefined value is requested.
   def get_file("GET", :undefined, req) do
     headers = [{"content-type", "text/html"}]
@@ -28,8 +51,8 @@ defmodule CowboyBlog.Handler do
     file_lists = File.ls! "priv/contents/"
     title = "Cowboy Blog"
     # Content stores the concatenated contents of all files within the "priv/contents".
-    content = print_articles file_lists, ""
-    body = EEx.eval_file "priv/themes/index.html.eex", [content: content, title: title]
+    content = print_articles file_lists
+    body = EEx.eval_file "priv/themes/index.html.eex", [content: content, title: title, body_header: body_header]
     # :cowboy_req.reply sends the response status line headers and body to the client.
     {:ok, resp} = :cowboy_req.reply(200, headers, body, req)
   end
@@ -38,23 +61,26 @@ defmodule CowboyBlog.Handler do
   # and parses it from markdown to HTML.
   def get_file("GET", param, req) do
     headers = [{"content-type", "text/html"}]
-    {:ok, file} = File.read "priv/contents/" <> param <> ".md"
+    {:ok, file} = File.read("priv/contents/" <> param <> ".md")
+    content = Earmark.to_html(file)
     title = String.capitalize(param) 
-    content = Earmark.to_html file
-    body = EEx.eval_file "priv/themes/index.html.eex", [content: content, title: title]
+    body = EEx.eval_file "priv/themes/index.html.eex", [content: content, title: title, body_header: body_header]
     {:ok, resp} = :cowboy_req.reply(200, headers, body, req)
   end
 
+  def print_articles(list, index_contents \\ "")
 
   # print_articles reads a list of files and parses each file to HTML.
   def print_articles [h|t], index_contents do
     {:ok, article} = File.read "priv/contents/" <> h
-    sliced = String.slice article, 0, 1000
-    # Marked stores the HTML parsed markdown files.
-    marked = Earmark.to_html sliced
+    # Marked stores the truncated and HTMP parsed article text.
+    marked = article
+             |> String.slice(0, 500)
+             |> Earmark.to_html()
     # File stores the filename minus the file extension.
-    file = Path.rootname(h)
-    more = "<a class='button' href='#{file}'>More</a><hr />"
+    file = Path.rootname(h) 
+    # More stores a link to the full article.
+    more = "<a class='button button-primary' href='#{file}'>More</a><hr />"
     print_articles t, index_contents <> marked <> more
   end
 
