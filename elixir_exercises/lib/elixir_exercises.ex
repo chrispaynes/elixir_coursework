3bc9bbbeb8998f3358b2bd407d1e58352338bc6a
chrispaynes <cdpaynes@gmail.com>
2016-12-08 22:29:32 -0600

    Unnests modules

---
+++ elixir_exercises/lib/elixir_exercises.ex
@@ -0,0 +1,12 @@
+defmodule ElixirExercises do
+
+  @moduledoc """
+    Module of Elixir code exercises
+  """
+
+  require ArgumentsOptional
+  require KaprekarsConstant
+  require Factorialize
+  require ZipDouble
+
+end
