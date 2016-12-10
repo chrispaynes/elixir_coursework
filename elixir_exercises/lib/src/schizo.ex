ec268ce39aacf07f63302d4e65dc5940611ee202
chrispaynes <cdpaynes@gmail.com>
2016-12-10 12:54:03 -0600

    Completes unvowel functionality

--- elixir_exercises/lib/src/schizo.ex
+++ elixir_exercises/lib/src/schizo.ex
@@ -1,11 +1,15 @@
 defmodule Schizo do
   def uppercase(string) do
-    string |> uppercase_every_other_word 
+    transform_every_other_word(string, &uppercaser(&1))
+  end
+  
+  def unvowel(string) do
+    transform_every_other_word(string, &remove_vowels(&1))
   end
 
-  def uppercase_every_other_word(string) do
+  def transform_every_other_word(string, transformation) do
     words_with_index = string |> String.split |> Enum.with_index
-    transform_words = Enum.map(words_with_index, &uppercaser(&1))
+    transform_words = Enum.map(words_with_index, transformation)
     Enum.join(transform_words, " ")
   end
 
@@ -16,8 +20,11 @@ defmodule Schizo do
     end
   end
 
-  def unvowel(string) do
-    string
+  def remove_vowels({word, index}) do
+    cond do
+      rem(index, 2) == 0 -> word
+      rem(index, 2) == 1 -> Regex.replace(~r/a|e|i|o|i|y/, word, "")
+    end
   end
 end
 
