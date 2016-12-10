1042832699d7782b7747f1e0fbc9abf5d3d3f856
chrispaynes <cdpaynes@gmail.com>
2016-12-10 13:13:51 -0600

    Removes unnecessary anonymous fn and moves to private fns

--- elixir_exercises/lib/src/schizo.ex
+++ elixir_exercises/lib/src/schizo.ex
@@ -7,24 +7,24 @@ defmodule Schizo do
     transform_every_other_word(string, &remove_vowels(&1))
   end
 
-  def transform_every_other_word(string, transformation) do
+  defp transform_every_other_word(string, transformation) do
     words_with_index = string |> String.split |> Enum.with_index
     transform_words = Enum.map(words_with_index, transformation)
     Enum.join(transform_words, " ")
   end
 
-  def transformer({word, index}, transformation) do
+  defp transformer({word, index}, transformation) do
     cond do
       rem(index, 2) == 0 -> word
       rem(index, 2) == 1 -> transformation.(word)
     end
   end
 
-  def uppercaser(input) do
-    transformer(input, fn(word) -> String.upcase(word) end)
+  defp uppercaser(input) do
+    transformer(input, &String.upcase(&1))
   end
 
-  def remove_vowels(input) do
+  defp remove_vowels(input) do
     transformer(input, fn(word) -> Regex.replace(~r/[aeiou]/, word, "") end)
   end
 end
