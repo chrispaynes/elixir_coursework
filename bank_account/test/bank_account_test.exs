b8aab9d6b2c9248a345061f13dac31a12f2165ff
chrispaynes <cdpaynes@gmail.com>
2016-12-21 17:10:19 -0600

    Creates BankAccount module and verification helper

--- bank_account/test/bank_account_test.exs
+++ bank_account/test/bank_account_test.exs
@@ -1,3 +1,9 @@
+defmodule BankAccount do
+  def start do
+  end
+
+end
+
 defmodule BankAccountTest do
   use ExUnit.Case
   doctest BankAccount
@@ -5,5 +11,10 @@ defmodule BankAccountTest do
   test "the bank account starts with a zero balance" do
     account = spawn_link(BankAccount, :start, [])
     verify_balance_is 0, account
+  end
+
+  def verify_balance_is(expected_balance, account) do
+    send(account, {:check_balance, self})
+    assert_receive {:balance, ^expected_balance}
   end
 end
