3e3acac04613b46669fa67ece658c98ad772169b
chrispaynes <cdpaynes@gmail.com>
2016-12-21 17:25:02 -0600

    Passes verify balance test

--- bank_account/test/bank_account_test.exs
+++ bank_account/test/bank_account_test.exs
@@ -1,7 +1,18 @@
 defmodule BankAccount do
   def start do
+    await
   end
 
+  def await do
+    receive do
+      {:check_balance, pid} -> get_balance(pid)
+    end
+    await
+  end
+
+  def get_balance(pid) do
+    send(pid, {:balance, 0})
+  end
 end
 
 defmodule BankAccountTest do
