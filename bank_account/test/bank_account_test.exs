d22e1978f5c40919e3633d8a13ccb9597f668160
chrispaynes <cdpaynes@gmail.com>
2016-12-21 20:46:45 -0600

    Adds account deposit functionality

--- bank_account/test/bank_account_test.exs
+++ bank_account/test/bank_account_test.exs
@@ -1,17 +1,31 @@
 defmodule BankAccount do
   def start do
-    await
+    await([])
   end
 
-  def await do
+  def await(events) do
     receive do
-      {:check_balance, pid} -> get_balance(pid)
+      {:check_balance, pid} -> get_balance(events, pid)
+      {:deposit_into_account, amount} -> events = deposit_into_account(amount, events)
     end
-    await
+    await(events)
   end
 
-  def get_balance(pid) do
-    send(pid, {:balance, 0})
+  def get_balance(events, pid) do
+    send(pid, {:balance, calculate_balance(events)})
+  end
+
+  defp calculate_balance(events) do
+    deposits = sum(events)
+    deposits
+  end
+
+  defp sum(events) do
+    Enum.reduce(events, 0, fn({_, amount}, acc) -> acc + amount end)
+  end
+
+  def deposit_into_account(amount, events) do
+    IO.inspect events ++ [{:deposit_into_account, amount}]
   end
 end
 
@@ -22,6 +36,12 @@ defmodule BankAccountTest do
   test "the bank account starts with a zero balance" do
     account = spawn_link(BankAccount, :start, [])
     verify_balance_is 0, account
+  end
+
+  test "the bank account accepts a deposit" do
+    account = spawn_link(BankAccount, :start, [])
+    send(account, {:deposit_into_account, 25})
+    verify_balance_is 25, account
   end
 
   def verify_balance_is(expected_balance, account) do
