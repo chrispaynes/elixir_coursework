6128b351e745ec1962b8e977442a4bd98684dd71
chrispaynes <cdpaynes@gmail.com>
2016-12-23 11:13:12 -0600

    Removes boilerplate from Public API

--- otp_server/test/fridge_server_test.exs
+++ otp_server/test/fridge_server_test.exs
@@ -5,6 +5,11 @@ defmodule FridgeServer do
     {:ok, state}
   end
 
+  def start_link(_, state, options \\ []) do
+    {:ok, fridge} = :gen_server.start_link(__MODULE__, state, options)
+    fridge
+  end
+
   def handle_call({:store, food_item}, _, state) do
     {:reply, :ok, [food_item|state]}
   end
@@ -23,20 +28,18 @@ defmodule FridgeServerTest do
   use ExUnit.Case, async: true
 
   test "the fridge accepts food" do
-    #{:ok, pid} = GenServer.start_link(Stack, [:hello])
-
-    {:ok, fridge} = :gen_server.start_link(FridgeServer, [], [])
+    fridge = FridgeServer.start_link(FridgeServer, [])
     assert :ok == :gen_server.call(fridge, {:store, :bacon})
   end
 
   test "can remove items from the fridge" do
-    {:ok, fridge} = :gen_server.start_link(FridgeServer, [], [])
+    fridge = FridgeServer.start_link(FridgeServer, [])
     :gen_server.call(fridge, {:store, :bacon})
     assert {:ok, :bacon} == :gen_server.call(fridge, {:take, :bacon})
   end
 
   test "cannot remove items from the fridge if they are not stored in the fridge" do
-    {:ok, fridge} = :gen_server.start_link(FridgeServer, [], [])
+    fridge = FridgeServer.start_link(FridgeServer, [])
     assert :not_found == :gen_server.call(fridge, {:take, :bacon})
   end
 end
